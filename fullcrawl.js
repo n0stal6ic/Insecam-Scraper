de02853703534831c036f6bee6c5ad9dda396661
n0stal6ic <owen2856@gmail.com>
2023-06-08 14:46:43 -0500

    Add files via upload

---
+++ fullcrawl.js
@@ -0,0 +1,39 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+let cams = [];
+
+async function fetchPage(i, callback) {
+  let req = await fetch(`http://insecam.org/en/bynew/?page=${i}`);
+  let res = await req.text();
+  let dom = new DOMParser().parseFromString(res, "text/html");
+
+  let thumbnails = dom.querySelectorAll(".thumbnail-item__wrap");
+  for (let thumbnail of thumbnails) {
+    let url = `http://insecam.org${thumbnail.attributes.href}`;
+    await callback(url);
+  }
+}
+
+async function fetchCam(cam) {
+    try {
+        let req = await fetch(cam);
+        let res = await req.text();
+        let dom = new DOMParser().parseFromString(res, "text/html");
+          
+        let element = dom.querySelector(".img-responsive.img-rounded.detailimage");
+        let url = new URL(element.attributes.src);
+        console.log(url.href);
+    } catch {
+        console.error(`failed fetching ${cam}`)
+    }
+}
+
+async function main() {
+    for(let i = 0; i < 1000; i++) {
+        setTimeout(() => {
+            fetchPage(i, url => fetchCam(url))
+        }, i * 200)
+    }
+}
+
+main()
